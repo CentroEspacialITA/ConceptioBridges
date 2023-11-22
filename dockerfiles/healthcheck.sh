5d2d440e1a212da7fe3598c43fb1f7b84feb45aa
mvccogo <mvccogo@gmail.com>
2023-11-22 15:26:04 -0300

    Fix variable definition in healthcheck

--- dockerfiles/healthcheck.sh
+++ dockerfiles/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-node_name_mqtt_mirror = "/mqtt_mirror_node"
-
+node_name_mqtt_mirror="/mqtt_mirror_node"
+source /opt/ros/$ROS_DISTRO/setup.bash
 node_list=$(ros2 node list)
 
 if [[ $node_list == *"$node_name_mqtt_mirror"*]]; then
