6f0efe5d18b5ff17fc62aac44090c48711c82419
mvccogo <mvccogo@gmail.com>
2023-11-22 15:40:12 -0300

    Add whitespace to end of statement

--- dockerfiles/healthcheck.sh
+++ dockerfiles/healthcheck.sh
@@ -4,7 +4,7 @@ node_name_mqtt_mirror="/mqtt_mirror_node"
 source /opt/ros/$ROS_DISTRO/setup.bash
 node_list=$(ros2 node list)
 
-if [[ $node_list == *"$node_name_mqtt_mirror"*]]; then
+if [[ "$node_list" == *"$node_name_mqtt_mirror"* ]]; then
     exit 0
 else
     exit 1
